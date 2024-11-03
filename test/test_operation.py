c034f21e79eec6861e979182c4c7869f371135a3
josie <latinue@gmail.com>
2024-11-03 03:56:12 -0500

    file updated

--- test/test_operation.py
+++ test/test_operation.py
@@ -4,7 +4,7 @@ def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
     assert add(7,2)==9
-    assert add(4,0)==6
+    assert add(4,0)==4
     
 def test_sub():
     assert sub(5,3)==2
